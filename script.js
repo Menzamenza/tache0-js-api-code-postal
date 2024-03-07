2f21758581ff23eba29855d996e9c535a7b12037
Menza <menzamenza90@gmail.com>
2024-03-07 14:24:17 +0000

    commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelector("#cp").addEventListener('input',function () {
+    if(this.value.length==5){
+        let url=`https://geo.api.gouv.fr/communes?codePostal=${this.value}`
+        fetch(url).then(( response)=>
+        response.json().then((data)=> {
+        console.log(data)
+        let affichage = '<ul>'
+        for (let ville of data) {
+            affichage+= `<li><strong>${ville.nom}</strong> - ${ville.population} hbts</li>`
+        }
+        affichage+='</ul>'
+        document.getElementById('ville').innerHTML= affichage
+        })
+        ).catch(err=>console.log('Erreur:'+ err))
+    }
+})
